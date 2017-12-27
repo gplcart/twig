68000297c098af6030b37e28efe232e96c713957
GplCart <gplcart.software@gmail.com>
2017-12-27 14:15:38 +0200

    Rename main module class to Module

--- Module.php
+++ Module.php
@@ -10,12 +10,12 @@
 namespace gplcart\modules\twig;
 
 use gplcart\core\Library,
-    gplcart\core\Module;
+    gplcart\core\Module as CoreModule;
 
 /**
  * Main class for Twig module
  */
-class Twig
+class Module
 {
 
     /**
@@ -37,10 +37,10 @@ class Twig
     protected $library;
 
     /**
-     * @param Module $module
+     * @param CoreModule $module
      * @param Library $library
      */
-    public function __construct(Module $module, Library $library)
+    public function __construct(CoreModule $module, Library $library)
     {
         $this->module = $module;
         $this->library = $library;
